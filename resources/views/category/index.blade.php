cf7c7477a2261cf5eb9ae3f28d3c058a7c9a2e10
Azizbek <azizbekvahidov@gmail.com>
2022-09-20 23:44:48 +0500

    add crud create and index page to category

---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,31 @@
+@extends("layout.main")
+
+@section("content")
+    <div>
+        <a href="{{ route("category.create") }}" class="btn btn-success">create</a>
+    </div>
+    <div class="col-8">
+        <table class="table table-bordered">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($categoryList as $category)
+                <tr>
+                    <td>{{$category->id}}</td>
+                    <td>{{$category->name}}</td>
+                    <td>
+                        <a href="{{ route("category.edit",$category->id) }}" class="btn btn-info">update</a>
+                        <a href="{{ route("category.delete",$category->id) }}" class="btn btn-danger">delete</a>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+
+        </table>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use \App\Http\Controllers\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group([
+    "prefix" => "category",
+    "as" => "category.",
+],function(){
+    Route::get("",[CategoryController::class, 'index'])->name("index");
+    Route::get("create",[CategoryController::class, 'create'])->name("create");
+    Route::put("store",[CategoryController::class, 'store'])->name("store");
+    Route::get("edit/{id}",[CategoryController::class, 'edit'])->name("edit");
+    Route::patch("update{id}",[CategoryController::class, 'update'])->name("update");
+    Route::delete("delete{id}",[CategoryController::class, 'delete'])->name("delete");
 });
